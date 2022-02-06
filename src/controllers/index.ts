7fd930e7e587f401130d2a4caab76b5fc1cfacbd
CH_Hwang <hch950627@naver.com>
2022-02-06 15:48:38 +0900

    [feat] github action

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response, NextFunction } from 'express';
 
 class Index {
   public getIndex = async (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send('hi');
+    res.status(200).send('hi, deploy');
   };
 }
 
