c56f1e83591956121de4c0fd9098e2fa080bb851
CH_Hwang <hch950627@naver.com>
2022-02-06 16:42:28 +0900

    [feat] github action

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -2,5 +2,7 @@
 
 if [ -d /home/ubuntu/linkgather/Back]; then
   sudo rm -rf /home/ubuntu/linkgather/Back
+  echo ">delete folder"
 fi
 sudo mkdir -vp /home/ubuntu/linkgather/Back
+echo ">mkdir"
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,7 @@
 
 cd /home/ubuntu/linkgather/Back/
 
+echo "> deploy"
+
 sudo pm2 kill
 sudo pm2 start ecosystem.config.js
