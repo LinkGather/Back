ba40964facd3dea9b8231f180b5e58d203b7ebbb
CH_Hwang <hch950627@naver.com>
2022-02-20 02:22:49 +0900

    [refactor] koa, ddd migration

--- src/controllers/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response, NextFunction } from 'express';
-
-class Index {
-  public getIndex = async (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send('hi, deploy');
-  };
-}
-
-export default new Index();
